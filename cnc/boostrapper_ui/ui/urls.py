00a0cb2cb1228447cb67880d826853793dbca585
Nathan <nembery@gmail.com>
2018-12-05 16:12:59 -0600

    initial cnc support

---
+++ cnc/boostrapper_ui/ui/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from ui.views import *
+
+app_name = 'bootstrapper'
+urlpatterns = [
+    path('', BootstrapWorkflowView.as_view()),
+    path('step02', BootstrapStep02View.as_view()),
+    path('step03', BootstrapStep03View.as_view()),
+    path('step04', BootstrapStep04View.as_view()),
+
+]
