3823a888a5cc3ae9dccf7df7a59cd0344a985339
Nathan <nembery@gmail.com>
2019-04-02 08:59:06 -0400

    revert lost changes

--- cnc/src/bootstrapper/views.py
+++ cnc/src/bootstrapper/views.py
@@ -1,16 +1,26 @@
-import json
 import re
 from base64 import urlsafe_b64encode
 
+import json
+import os
+import re
+import shutil
+from base64 import urlsafe_b64encode
+from pathlib import Path
+
 import requests
 from django import forms
+from django.conf import settings
 from django.contrib import messages
 from django.shortcuts import render, HttpResponseRedirect, HttpResponse
+from django.views.generic import RedirectView
 
 from pan_cnc.lib import cnc_utils
+from pan_cnc.lib import git_utils
 from pan_cnc.lib import pan_utils
 from pan_cnc.lib import snippet_utils
-from pan_cnc.views import CNCBaseFormView
+from pan_cnc.lib.exceptions import TargetConnectionException
+from pan_cnc.views import CNCBaseFormView, CNCView, CNCBaseAuth
 
 
 class BootstrapWorkflowView(CNCBaseFormView):
@@ -38,7 +48,7 @@ class DynamicContentView(BootstrapWorkflowView):
         if self.get_value_from_workflow('include_dynamic_content', 'no') == 'yes':
             return HttpResponseRedirect('download_content')
         else:
-            return HttpResponseRedirect('complete')
+            return HttpResponseRedirect('configure_management')
 
 
 class DownloadDynamicContentView(CNCBaseFormView):
@@ -66,7 +76,7 @@ class DownloadDynamicContentView(CNCBaseFormView):
             filename = resp.headers['Content-Disposition'].split('=')[1]
             messages.add_message(self.request, messages.INFO, f'Downloaded Dynamic Content file: {filename}')
 
-        return HttpResponseRedirect('complete')
+        return HttpResponseRedirect('configure_management')
 
 
 class GetCloudAuthView(BootstrapWorkflowView):
@@ -108,10 +118,14 @@ class BootstrapStep03View(BootstrapWorkflowView):
         target_ip = self.get_value_from_workflow('TARGET_IP', '')
         target_username = self.get_value_from_workflow('TARGET_USERNAME', '')
         target_password = self.get_value_from_workflow('TARGET_PASSWORD', '')
-        p = pan_utils.panorama_login(target_ip, target_username, target_password)
+        p = pan_utils.panos_login(target_ip, target_username, target_password)
+        if p is None:
+            results = dict()
+            results['results'] = 'Error, Could not contact Panorama'
+            return render(self.request, 'pan_cnc/results.html', context=results)
         try:
             r = pan_utils.get_vm_auth_key_from_panorama()
-        except BaseException:
+        except TargetConnectionException:
             print('Could not get vm auth key from panorama')
             messages.add_message(self.request, messages.ERROR, 'Could not contact Panorama!')
             results = dict()
@@ -137,7 +151,26 @@ class BootstrapStep04View(BootstrapWorkflowView):
         if self.get_value_from_workflow('include_panorama', 'no') == 'no':
             return HttpResponseRedirect('choose_bootstrap')
         else:
+            return HttpResponseRedirect('configure_management')
+
+
+class ConfigureManagementView(BootstrapWorkflowView):
+    title = 'Configure Management Connectivity'
+    fields_to_render = ['network_type']
+
+    def form_valid(self, form):
+        if self.get_value_from_workflow('network_type', 'dhcp-client') == 'dhcp-client':
             return HttpResponseRedirect('complete')
+        else:
+            return HttpResponseRedirect('configure_management_ip')
+
+
+class ConfigureManagementStaticView(BootstrapWorkflowView):
+    title = 'Configure Management Static IP Address'
+    fields_to_render = ['ipv4_mgmt_address', 'ipv4_mgmt_netmask', 'ipv4_default_gateway']
+
+    def form_valid(self, form):
+        return HttpResponseRedirect('complete')
 
 
 class ChooseBootstrapXmlView(BootstrapWorkflowView):
@@ -163,8 +196,9 @@ class ChooseBootstrapXmlView(BootstrapWorkflowView):
 
         # let's sort the list by the label attribute (index 1 in the tuple)
         choices_list = sorted(choices_list, key=lambda k: k[1])
-        choices_list.insert(0, ('bootstrap_xml', 'Use Default Bootstrap'))
-        choices_list.insert(1, ('upload', 'Upload Custom Bootstrap'))
+        choices_list.insert(0, ('none', 'Do not include a bootstrap'))
+        choices_list.insert(1, ('bootstrap_xml', 'Use Default Bootstrap'))
+        choices_list.insert(2, ('upload', 'Upload Custom Bootstrap'))
 
         dynamic_form.fields['custom_bootstrap'] = forms.ChoiceField(label='Custom Bootstrap',
                                                                     choices=tuple(choices_list))
@@ -185,7 +219,9 @@ class ChooseBootstrapXmlView(BootstrapWorkflowView):
 
         if cb == 'upload':
             return HttpResponseRedirect('upload_bootstrap')
-
+        elif cb == 'none':
+            self.save_value_to_workflow('bootstrap_string', '')
+            return HttpResponseRedirect('include_content')
         else:
             # custom bootstrap is set to some other value from a snippet
             return HttpResponseRedirect('configure_bootstrap')
@@ -302,3 +338,127 @@ class CompleteWorkflowView(BootstrapWorkflowView):
             results['results'] += resp.text
 
             return render(self.request, 'pan_cnc/results.html', context=results)
+
+
+class ImportGitRepoView(CNCBaseFormView):
+    # define initial dynamic form from this snippet metadata
+    snippet = 'import_repo'
+    app_dir = 'bootstrapper'
+    next_url = '/bootstrapper/repos'
+
+    def get_snippet(self):
+        # always return the hard configured snippet
+        return self.snippet
+
+    # once the form has been submitted and we have all the values placed in the workflow, execute this
+    def form_valid(self, form):
+        workflow = self.get_workflow()
+
+        # get the values from the user submitted form here
+        url = workflow.get('url')
+        branch = workflow.get('branch')
+        repo_name = workflow.get('repo_name')
+        # FIXME - Ensure repo_name is unique
+
+        # we are going to keep the snippets in the snippets dir in the panhandler app
+        # get the dir where all apps are installed
+        src_dir = settings.SRC_PATH
+        # get the panhandler app dir
+        panhandler_dir = os.path.join(src_dir, self.app_dir)
+        # get the snippets dir under that
+        snippets_dir = os.path.join(panhandler_dir, 'snippets')
+        # figure out what our new repo / snippet dir will be
+        new_repo_snippets_dir = os.path.join(snippets_dir, repo_name)
+
+        # where to clone from
+        clone_url = url
+        if 'github' in url.lower():
+            details = git_utils.get_repo_upstream_details(repo_name, url)
+            if 'clone_url' in details:
+                clone_url = details['clone_url']
+
+        if not git_utils.clone_repo(new_repo_snippets_dir, repo_name, clone_url, branch):
+            messages.add_message(self.request, messages.ERROR, 'Could not Import Repository')
+        else:
+            print('Invalidating snippet cache')
+            snippet_utils.invalidate_snippet_caches()
+
+            messages.add_message(self.request, messages.INFO, 'Imported Repository Successfully')
+
+        # return render(self.request, 'pan_cnc/results.html', context)
+        return HttpResponseRedirect(self.next_url)
+
+
+class UpdateGitRepoView(CNCBaseAuth, RedirectView):
+    next_url = '/bootstrapper/repos'
+    app_dir = 'bootstrapper'
+
+    def get_redirect_url(self, *args, **kwargs):
+        repo_name = kwargs['repo_name']
+        # we are going to keep the snippets in the snippets dir in the panhandler app
+        # get the dir where all apps are installed
+        src_dir = settings.SRC_PATH
+        # get the panhandler app dir
+        panhandler_dir = os.path.join(src_dir, self.app_dir)
+        # get the snippets dir under that
+        snippets_dir = os.path.join(panhandler_dir, 'snippets')
+        repo_dir = os.path.join(snippets_dir, repo_name)
+
+        msg = git_utils.update_repo(repo_dir)
+        if 'Error' in msg:
+            level = messages.ERROR
+        else:
+            print('Invalidating snippet cache')
+            snippet_utils.invalidate_snippet_caches()
+            level = messages.INFO
+
+        messages.add_message(self.request, level, msg)
+        return self.next_url
+
+
+class RemoveGitRepoView(CNCBaseAuth, RedirectView):
+    next_url = '/bootstrapper/repos'
+    app_dir = 'bootstrapper'
+
+    def get_redirect_url(self, *args, **kwargs):
+        repo_name = kwargs['repo_name']
+        # we are going to keep the snippets in the snippets dir in the panhandler app
+        # get the dir where all apps are installed
+        src_dir = settings.SRC_PATH
+        # get the panhandler app dir
+        panhandler_dir = os.path.join(src_dir, self.app_dir)
+        # get the snippets dir under that
+        snippets_dir = os.path.join(panhandler_dir, 'snippets')
+        repo_dir = os.path.abspath(os.path.join(snippets_dir, repo_name))
+
+        if snippets_dir in repo_dir:
+            print(f'Removing repo {repo_name}')
+            print('Invalidating snippet cache')
+            snippet_utils.invalidate_snippet_caches()
+            shutil.rmtree(repo_dir)
+
+        messages.add_message(self.request, messages.SUCCESS, 'Repo Successfully Removed')
+        return self.next_url
+
+
+class ListGitReposView(CNCView):
+    template_name = 'bootstrapper/repos.html'
+    app_dir = 'bootstrapper'
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        snippets_dir = Path(os.path.join(settings.SRC_PATH, self.app_dir, 'snippets'))
+        repos = list()
+        for d in snippets_dir.rglob('./*'):
+            # git_dir = os.path.join(d, '.git')
+            git_dir = d.joinpath('.git')
+            if git_dir.exists() and git_dir.is_dir():
+                print(d)
+                repo_name = os.path.basename(d)
+                repo_detail = git_utils.get_repo_details(repo_name, d)
+                repos.append(repo_detail)
+                continue
+
+        context['repos'] = repos
+        return context
